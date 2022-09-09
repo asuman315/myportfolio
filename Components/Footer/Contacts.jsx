62692f81b65d02c63d87a19fcd892312cbe985d2
asumanwebdev <asumanwebdev@gmail.com>
2022-09-10 00:46:36 +0300

    fix errors

--- Components/Footer/Contacts.jsx
+++ Components/Footer/Contacts.jsx
@@ -5,7 +5,7 @@ const Contacts = () => {
   return (
     <div
       name='Contact'
-      className='bg-primary-1 p-4 w-autofill w-[fit-content] rounded-sm mx-auto mt-6 h-[fit-content]'
+      className='bg-primary-1 p-4 w-autofill w-[fit-content] rounded-sm mx-auto mt-6 h-[fit-content]'>
       <Slide left>
         <div className='pt-2'>
           <p>Email</p>
